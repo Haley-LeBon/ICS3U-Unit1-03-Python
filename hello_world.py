133807a7241c0557b9a3c4166a880c031f7207e1
Haley Le Bon <haley.lebon@mths.ca>
2022-09-16 15:25:27 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Haley Lebon
+# Created on: Sept 2020
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    #this fuctio prints to the console
+
+    print("Hello,World!")
+
+    print("n\Done.")
+
+if __name__ == "__main__":
+    main()
